3a3cfda7941a6916046450b7db48ff8a78987971
Abdulsks <95519458+AbdulSKS@users.noreply.github.com>
2023-12-27 04:23:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from app_service import AppService
+import json
 
 app = Flask(__name__)
+appService = AppService();
+
 
 @app.route('/')
 def home():
-    return 'Hello, CI/CD Pipeline with Terraform and Python on AWS!'
+    return "App Works!!!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.route('/api/tasks')
+def tasks():
+    return appService.get_tasks()
+
+@app.route('/api/task', methods=['POST'])
+def create_task():
+    request_data = request.get_json()
+    task = request_data['task']
+    return appService.create_task(task)
+
+
+@app.route('/api/task', methods=['PUT'])
+def update_task():
+    request_data = request.get_json()
+    return appService.update_task(request_data['task'])
+
+
+@app.route('/api/task/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    return appService.delete_task(id)
