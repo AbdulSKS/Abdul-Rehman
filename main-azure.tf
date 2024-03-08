95d06fffe24a3cf52af8f9c4d7ec5ae4d5c7ea9b
Abdulsks <95519458+AbdulSKS@users.noreply.github.com>
2024-03-08 22:21:28 +0530

    Update main-azure.tf

--- main-azure.tf
+++ main-azure.tf
@@ -1,1 +1,68 @@
-df
+provider "azurerm" {
+  subscription_id = "your_subscription_id"
+  tenant_id       = "your_tenant_id"
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "central india"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "test-vnet"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "test477_z1"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "Compute-instance-by-AbdulRehman"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = "central india"
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "adminuser"
+  admin_password                  = "@@12345678"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
